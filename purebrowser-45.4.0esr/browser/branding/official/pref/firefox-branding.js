c0de808e30369900d864116f794a15841ac3f798
Hema Prathaban <hema.prathaban@puri.sm>
2016-11-09 07:47:05 +0100

    Branding-name: firefox brands (name and url) are changed as purism brands
    Signed-off-by: Hema Prathaban <hema.prathaban@puri.sm>

--- purebrowser-45.4.0esr/browser/branding/official/pref/firefox-branding.js
+++ purebrowser-45.4.0esr/browser/branding/official/pref/firefox-branding.js
@@ -3,8 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 pref("startup.homepage_override_url", "");
-pref("startup.homepage_welcome_url", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/");
-pref("startup.homepage_welcome_url.additional", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/learnmore/");
+pref("startup.homepage_welcome_url", "https://puri.sm");
+pref("startup.homepage_welcome_url.additional", "https://duckduckgo.com");
 // Interval: Time between checks for a new version (in seconds)
 pref("app.update.interval", 43200); // 12 hours
 // The time interval between the downloading of mar file chunks in the
@@ -14,10 +14,10 @@ pref("app.update.download.backgroundInterval", 60);
 pref("app.update.promptWaitTime", 172800);
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
-pref("app.update.url.manual", "https://www.mozilla.org/firefox/");
+pref("app.update.url.manual", "https://puri.sm");
 // A default value for the "More information about this update" link
 // supplied in the "An update is available" page of the update wizard. 
-pref("app.update.url.details", "https://www.mozilla.org/%LOCALE%/firefox/notes");
+pref("app.update.url.details", "https://puri.sm");
 
 // The number of days a binary is permitted to be old
 // without checking for an update.  This assumes that
@@ -35,3 +35,27 @@ pref("browser.search.param.yahoo-fr-ja", "mozff");
 // Number of usages of the web console or scratchpad.
 // If this is less than 5, then pasting code into the web console or scratchpad is disabled
 pref("devtools.selfxss.count", 0);
+lockPref("startup.homepage_override_url","https://duckduckgo.com");
+lockPref("startup.homepage_welcome_url","https://duckduckgo.com");
+pref("browser.search.defaultenginename",      "DuckDuckGo");
+pref("browser.search.order.1",                "data:text/plain,browser.search.order.1=DuckDuckGo");
+pref("browser.search.order.2",                "data:text/plain,browser.search.order.2=DuckDuckGo");
+ pref("browser.search.order.3",                "data:text/plain,browser.search.order.3=DuckDuckGo");
+// // Market-specific search defaults (US market only)
+pref("browser.search.geoSpecificDefaults", true);
+pref("browser.search.defaultenginename.US",      "data:text/plain,browser.search.defaultenginename.US=DuckDuckGo");
+pref("browser.search.order.US.1",                "data:text/plain,browser.search.order.US.1=DuckDuckGo");
+pref("browser.search.order.US.2",                "data:text/plain,browser.search.order.US.2=DuckDuckGo");
+pref("browser.search.order.US.3",                "data:text/plain,browser.search.order.US.3=DuckDuckGo");
+pref("datareporting.healthreport.service.firstRun", false);
+pref("datareporting.healthreport.service.enabled", false);
+pref("browser.search.suggest.enabled", false);
+pref("browser.urlbar.suggest.searches", true);
+pref("browser.urlbar.userMadeSearchSuggestionsChoice", false);
+pref("browser.urlbar.suggest.bookmark", false);
+pref("browser.urlbar.suggest.history", false);
+pref("browser.urlbar.suggest.history.onlyTyped", true);
+pref("browser.urlbar.suggest.openpage", true);
+pref("xpinstall.signatures.required",false);
+pref("datareporting.healthreport.uploadEnabled", false);
+pref("datareporting.healthreport.infoURL", "https://puri.sm");
