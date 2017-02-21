2031ca3620b5f58e2f3369910d7ebc290bccff12
Hema Prathaban <hema.prathaban@puri.sm>
2017-02-21 01:25:49 -0500

    Remove bash as default editor

--- purebrowser-45.4.0esr/debian/vendor.js
+++
@@ -1,6 +0,0 @@
-pref("distribution.searchplugins.defaultLocale", "en-US");
-// Forbid application updates
-lockPref("app.update.enabled", false);
-
-pref("devtools.webide.autoinstallADBHelper", false);
-pref("devtools.webide.autoinstallFxdtAdapters", false);
